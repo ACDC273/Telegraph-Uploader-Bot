3c114c7832c4517ef5f04e51f8f3f00313596d93
ACDC273 <90915515+ACDC273@users.noreply.github.com>
2023-11-28 10:05:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 Bot = Client(
     "Telegraph Uploader Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=int(os.environ.get('API_ID')),
     api_hash=os.environ.get("API_HASH")
 )
 
